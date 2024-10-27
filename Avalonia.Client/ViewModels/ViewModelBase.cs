eb0048277ca6c3440d5a89c57bc602d429323326
onomylock <onomylock@outlook.com>
2024-10-27 17:30:10 +0300

    init avalonia

---
+++ Avalonia.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia.Client.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Avalonia.Client/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avalonia.Client.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
